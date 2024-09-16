5900cd816cbbbfb67a7d4e5d718a8e0a385de91b
xmartina <martinsprince126@gmail.com>
2024-09-16 23:42:21 +0100

    d

---
+++ home/departments.php
@@ -0,0 +1,206 @@
+<!-- Services -->
+<section class="section-service pt-122 pb-130 bg-2">
+    <div class="tf-container">
+        <div class="heading-title center mb-60">
+            <span class="sub-title texts-blue font-man wow fadeInUpSmall" data-wow-delay=".2s">Popular Services</span>
+            <h2 class="title wow fadeInUpSmall" data-wow-delay=".3s">Services We Provide</h2>
+        </div>
+        <div class="service-grid mb-40 wow fadeInUpSmall" data-wow-delay=".3s">
+            <div class="icon-box1">
+                <div class="icon-box1-visible flex-two">
+                    <div class="content">
+                        <span class="number mb-15">01</span>
+                        <h3 class="title mb-47"><a href="service-details.html">UX/UI Design</a></h3>
+                        <div class="icon">
+                            <i class="icon-Icon11"></i>
+                        </div>
+                    </div>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link"><i class="icon-angle-downs"></i></a>
+                    </div>
+                </div>
+                <div class="icon-box1-hide">
+                    <div class="flex-one">
+                        <div class="icon">
+                            <i class="icon-Icon11"></i>
+                        </div>
+                        <span class="number">01</span>
+                    </div>
+                    <h3 class="title"><a href="service-details.html">UX/UI Design</a></h3>
+                    <p class="des">Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
+                        laudantium</p>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link">Read More<i
+                                class="icon-right-icon"></i></a>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="icon-box1">
+                <div class="icon-box1-visible flex-two">
+                    <div class="content">
+                        <span class="number mb-15">02</span>
+                        <h3 class="title mb-47"><a href="service-details.html">IT Management</a></h3>
+                        <div class="icon">
+                            <i class="icon-self-development-2"></i>
+                        </div>
+                    </div>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link"><i class="icon-angle-downs"></i></a>
+
+                    </div>
+                </div>
+                <div class="icon-box1-hide">
+                    <div class="flex-one">
+                        <div class="icon">
+                            <i class="icon-self-development-2"></i>
+                        </div>
+                        <span class="number">02</span>
+                    </div>
+                    <h3 class="title"><a href="service-details.html">IT Management</a></h3>
+                    <p class="des">Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
+                        laudantium</p>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link">Read More<i
+                                class="icon-right-icon"></i></a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="icon-box1">
+                <div class="icon-box1-visible flex-two">
+                    <div class="content">
+                        <span class="number mb-15">03</span>
+                        <h3 class="title mb-47"><a href="service-details.html">Data Security</a></h3>
+                        <div class="icon">
+                            <i class="icon-data-management-1"></i>
+                        </div>
+                    </div>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link"><i class="icon-angle-downs"></i></a>
+
+                    </div>
+                </div>
+                <div class="icon-box1-hide">
+                    <div class="flex-one">
+                        <div class="icon">
+                            <i class="icon-data-management-1"></i>
+                        </div>
+                        <span class="number">03</span>
+                    </div>
+                    <h3 class="title"><a href="service-details.html">Data Security</a></h3>
+                    <p class="des">Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
+                        laudantium</p>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link">Read More<i
+                                class="icon-right-icon"></i></a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="icon-box1">
+                <div class="icon-box1-visible flex-two">
+                    <div class="content">
+                        <span class="number mb-15">04</span>
+                        <h3 class="title mb-47"><a href="service-details.html">Infrastructure Plan </a></h3>
+                        <div class="icon">
+                            <i class="icon-planning-2"></i>
+                        </div>
+                    </div>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link"><i class="icon-angle-downs"></i></a>
+
+                    </div>
+                </div>
+                <div class="icon-box1-hide">
+                    <div class="flex-one">
+                        <div class="icon">
+                            <i class="icon-planning-2"></i>
+                        </div>
+                        <span class="number">04</span>
+                    </div>
+                    <h3 class="title"><a href="service-details.html">Infrastructure Plan</a></h3>
+                    <p class="des">Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
+                        laudantium</p>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link">Read More<i
+                                class="icon-right-icon"></i></a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="icon-box1">
+                <div class="icon-box1-visible flex-two">
+                    <div class="content">
+                        <span class="number mb-15">05</span>
+                        <h3 class="title mb-47"><a href="service-details.html">Firewall Advancement </a></h3>
+                        <div class="icon">
+                            <i class="icon-start-up"></i>
+                        </div>
+                    </div>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link"><i class="icon-angle-downs"></i></a>
+
+                    </div>
+                </div>
+                <div class="icon-box1-hide">
+                    <div class="flex-one">
+                        <div class="icon">
+                            <i class="icon-start-up"></i>
+                        </div>
+                        <span class="number">05</span>
+                    </div>
+                    <h3 class="title"><a href="service-details.html">Firewall Advancement</a></h3>
+                    <p class="des">Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
+                        laudantium</p>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link">Read More<i
+                                class="icon-right-icon"></i></a>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="icon-box1">
+                <div class="icon-box1-visible flex-two">
+                    <div class="content">
+                        <span class="number mb-15">06</span>
+                        <h3 class="title mb-47"><a href="service-details.html">Desktop Computing</a></h3>
+                        <div class="icon">
+                            <i class="icon-cloud-network"></i>
+                        </div>
+                    </div>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link"><i class="icon-angle-downs"></i></a>
+
+                    </div>
+                </div>
+                <div class="icon-box1-hide">
+                    <div class="flex-one">
+                        <div class="icon">
+                            <i class="icon-cloud-network"></i>
+                        </div>
+                        <span class="number">06</span>
+                    </div>
+                    <h3 class="title"><a href="service-details.html">Desktop Computing</a></h3>
+                    <p class="des">Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
+                        laudantium</p>
+                    <div class="button-icon-box">
+                        <a href="service-details.html" class="service-link">Read More<i
+                                class="icon-right-icon"></i></a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+        <div class="center wow fadeInUpSmall" data-wow-delay=".3s">
+            <div class="service-cta">
+                <img src="<?=$siteUrl?>assets/images/page/chat1.png" alt="image">
+                <p class="font-man">Get Free Consultations For Tech Solutions</p>
+                <a href="#">Get A Quote <i class="icon-right-icon"></i></a>
+            </div>
+        </div>
+    </div>
+</section>
--- home/index.php
+++ home/index.php
@@ -2,4 +2,5 @@
 <?php
 include_once (__DIR__ . '/slider.php');
 include_once (__DIR__ . '/partner.php');
+include_once (__DIR__ . '/departments.php');
 ?>
