e3d2c5fecf2ef67c460d6e71a2e52262ab4ed1b0
xmartina <martinsprince126@gmail.com>
2024-09-16 20:15:51 +0100

    d

--- functions/home_function.php
+++ functions/home_function.php
@@ -1,4 +1,5 @@
 <?php
 include_once (__DIR__ . '/main.php');
-?>
 
+$sql = "SELECT * FROM front_slider"; // Adjust the table name if different
+$result = $conn->query($sql);
--- home/slider.php
+++ home/slider.php
@@ -1,122 +1,43 @@
 <!--  Main Slider -->
 <section class="swiper slider-Swiper-it relative">
     <div class="swiper-wrapper">
+        <?php
+        while ($row = $result->fetch_assoc()) {
+        // Use the fetched data to populate the HTML template
+        ?>
         <div class="swiper-slide">
             <div class="slider-it-main relative over overflow-hiden">
                 <div class="slider-image-it">
-                    <img src="<?=$siteUrl?>assets/images/slide/slider1lider1.jpg" alt="">
+                    <!-- Use the slider_img column from the database for the image source -->
+                    <img src="<?=$siteUrl?>assets/images/slide/<?= htmlspecialchars($row['slider_img']) ?>" alt="">
                 </div>
-                <span class="it-solution fw-600">IT Solutions</span>
-                <!--                    <img src="--><?php //=$siteUrl?><!--assets/images/slide/mask.png" alt="image" class="mask-slider">-->
+                <span class="it-solution fw-600"><?= htmlspecialchars($row['bottom_title']) ?></span>
+                <!-- Example for dynamically adding sub-title and other fields -->
                 <div class="tf-container">
                     <div class="slider-content-it relative z-index-3">
                         <div class="sub-title-slider wow fadeInUpSmall">
-                            <i class="icon-angle-downs "></i>
-                            <span class="fw-600 text-white font-man">
-                                                    5m+ Trusted Our Clients</span>
+                            <i class="icon-angle-downs"></i>
+                            <span class="fw-600 text-white font-man"><?= htmlspecialchars($row['sub_title']) ?></span>
                         </div>
-                        <h1 class="title-slider text-white wow fadeInUpSmall" data-wow-delay=".2s">Modern & Digital <br> IT Solutions For
-                            Your
-                            <br> Tech Business
+                        <h1 class="title-slider text-white wow fadeInUpSmall" data-wow-delay=".2s">
+                            <?= htmlspecialchars($row['title']) ?>
                         </h1>
-                        <p class="des text-white fw-500 mb-40 wow fadeInUpSmall" data-wow-delay=".3s">25+ Years Of Experience In Tech
-                            Services
+                        <p class="des text-white fw-500 mb-40 wow fadeInUpSmall" data-wow-delay=".3s">
+                            <?= htmlspecialchars($row['inner_big_text']) ?>
                         </p>
                         <div class="btn-main wow fadeInUpSmall" data-wow-delay=".4s">
-                            <a href="service1.html" class="button-src ">Explore Our Service
-                                <i class="icon-angle-right"></i></a>
+                            <a href="<?= htmlspecialchars($row['button_link']) ?>" class="button-src">
+                                <?= htmlspecialchars($row['button_text']) ?>
+                                <i class="icon-angle-right"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="swiper-slide">
-            <div class="slider-it-main relative over overflow-hiden">
-                <div class="slider-image-it">
-                    <img src="<?=$siteUrl?>assets/images/slide/slider1lider2.jpg" alt="">
-                </div>
-                <span class="it-solution fw-600">IT Solutions</span>
-                 
-                <div class="tf-container">
-                    <div class="slider-content-it relative z-index-3">
-                        <div class="sub-title-slider wow fadeInUpSmall">
-                            <i class="icon-angle-downs "></i>
-                            <span class="fw-600 text-white font-man">
-                                                    5m+ Trusted Our Clients</span>
-                        </div>
-                        <h1 class="title-slider text-white wow fadeInUpSmall" data-wow-delay=".2s">Modern & Digital <br> IT Solutions For
-                            Your
-                            <br> Tech Business
-                        </h1>
-                        <p class="des text-white fw-500 mb-40 wow fadeInUpSmall" data-wow-delay=".3s">25+ Years Of Experience In Tech
-                            Services
-                        </p>
-                        <div class="btn-main wow fadeInUpSmall" data-wow-delay=".4s">
-                            <a href="service2.html" class="button-src">Explore Our Service <i
-                                    class="icon-angle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="swiper-slide">
-            <div class="slider-it-main relative over overflow-hiden">
-                <div class="slider-image-it">
-                    <img src="<?=$siteUrl?>assets/images/slide/slider1lider2.jpg" alt="">
-                </div>
-                <span class="it-solution fw-600">IT Solutions</span>
-                 
-                <div class="tf-container">
-                    <div class="slider-content-it relative z-index-3">
-                        <div class="sub-title-slider wow fadeInUpSmall">
-                            <i class="icon-angle-downs "></i>
-                            <span class="fw-600 text-white font-man">
-                                                    5m+ Trusted Our Clients</span>
-                        </div>
-                        <h1 class="title-slider text-white wow fadeInUpSmall" data-wow-delay=".2s">Modern & Digital <br> IT Solutions For
-                            Your
-                            <br> Tech Business
-                        </h1>
-                        <p class="des text-white fw-500 mb-40 wow fadeInUpSmall" data-wow-delay=".3s">25+ Years Of Experience In Tech
-                            Services
-                        </p>
-                        <div class="btn-main wow fadeInUpSmall" data-wow-delay=".4s">
-                            <a href="service2.html" class="button-src">Explore Our Service <i
-                                    class="icon-angle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="swiper-slide">
-            <div class="slider-it-main relative over overflow-hiden">
-                <div class="slider-image-it">
-                    <img src="<?=$siteUrl?>assets/images/slide/slider1lider2.jpg" alt="">
-                </div>
-                <span class="it-solution fw-600">IT Solutions</span>
-                 
-                <div class="tf-container">
-                    <div class="slider-content-it relative z-index-3">
-                        <div class="sub-title-slider wow fadeInUpSmall">
-                            <i class="icon-angle-downs "></i>
-                            <span class="fw-600 text-white font-man">
-                                                    5m+ Trusted Our Clients</span>
-                        </div>
-                        <h1 class="title-slider text-white wow fadeInUpSmall" data-wow-delay=".2s">Modern & Digital <br> IT Solutions For
-                            Your
-                            <br> Tech Business
-                        </h1>
-                        <p class="des text-white fw-500 mb-40 wow fadeInUpSmall" data-wow-delay=".3s">25+ Years Of Experience In Tech
-                            Services
-                        </p>
-                        <div class="btn-main wow fadeInUpSmall" data-wow-delay=".4s">
-                            <a href="service2.html" class="button-src">Explore Our Service <i
-                                    class="icon-angle-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php
+}
+?>
     </div>
     <div class="button-swiper">
         <div class="swiper-button-next"></div>
