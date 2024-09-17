9088d7be27e12eeefa3836f86be7512603b422d6
xmartina <martinsprince126@gmail.com>
2024-09-17 17:29:44 +0100

    f

--- home/departments.php
+++ home/departments.php
@@ -10,17 +10,19 @@
             if ($dep_result->num_rows > 0) {
                 $i = 1; // Initialize counter for numbering
                 while ($row = $dep_result->fetch_assoc()) {
+                    // Extract book_id for easier access in the HTML below
+                    $book_id = $row['book_id'];
                     echo '<div class="icon-box1">
             <div class="icon-box1-visible flex-two">
                 <div class="content">
                     <span class="number mb-15">' . str_pad($i, 2, '0', STR_PAD_LEFT) . '</span>
-                    <h3 class="title mb-47"><a href="department?book_id=' . $row['book_id'] . '">' . htmlspecialchars($row['dep_name']) . '</a></h3>
+                    <h3 class="title mb-47"><a href="department?book_id=' . $book_id . '">' . htmlspecialchars($row['dep_name']) . '</a></h3>
                     <div class="icon">
                         <i class="icon-Icon11"></i>
                     </div>
                 </div>
                 <div class="button-icon-box">
-                    <a href="department?book_id=' . $row['book_id'] . '" class="service-link"><i class="icon-angle-downs"></i></a>
+                    <a href="department?book_id=<?= $book_id ?>" class="service-link"><i class="icon-angle-downs"></i></a>
                 </div>
             </div>
             <div class="icon-box1-hide">
@@ -30,19 +32,20 @@
                     </div>
                     <span class="number">' . str_pad($i, 2, '0', STR_PAD_LEFT) . '</span>
                 </div>
-                <h3 class="title"><a href="department?book_id=' . $row['book_id'] . '">' . htmlspecialchars($row['dep_name']) . '</a></h3>
-                <p class="des">' . htmlspecialchars($row['dep_short_dec']) . '</p>
+                <h3 class="title"><a href="department?book_id=<?= $book_id ?>"><?= htmlspecialchars($row['dep_name']) ?></a></h3>
+                <p class="des"><?= htmlspecialchars($row['dep_short_dec']) ?></p>
                 <div class="button-icon-box">
-                    <a href="department?book_id=' . $row['book_id'] . '" class="service-link">Read More<i class="icon-right-icon"></i></a>
+                    <a href="department?book_id=<?= $book_id ?>" class="service-link">Read More<i class="icon-right-icon"></i></a>
                 </div>
             </div>
         </div>';
-                    $i++; // Increment counter
-                }
+        $i++; // Increment counter
+    }
             } else {
                 echo "0 results";
             }
             ?>
+
 
         </div>
         <div class="center wow fadeInUpSmall" data-wow-delay=".3s">
