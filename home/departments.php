51f7113b276d5516c2c11311ba58fbbc2c511c6a
xmartina <martinsprince126@gmail.com>
2024-09-17 17:46:12 +0100

    f

--- home/departments.php
+++ home/departments.php
@@ -50,7 +50,7 @@
             <div class="service-cta">
 <!--                <img src="--><?php //=$siteUrl?><!--assets/images/page/chat1.png" alt="image">-->
                 <p class="font-man">We have More</p>
-                <a href="#">View All Departments <i class="icon-right-icon"></i></a>
+                <a href="<?=$siteUrl?>pages/department/all">View All Departments <i class="icon-right-icon"></i></a>
             </div>
         </div>
     </div>
