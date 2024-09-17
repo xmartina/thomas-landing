6d68ed14037586c6cf41afe716df296a229dcabf
xmartina <martinsprince126@gmail.com>
2024-09-17 17:58:47 +0100

    f

--- pages/department/page/index.php
+++ pages/department/page/index.php
@@ -1,6 +1,6 @@
 <?php
-$page_name = $dep_name;
 include_once (__DIR__ . '/../../../functions/department_function.php');
+$page_name = $dep_name;
 include_once (__DIR__ . '/../../../partials/header.php');
 ?>
 
