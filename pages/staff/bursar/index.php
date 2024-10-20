bf77b8590670db7bf9367d01bfd663a61d977f72
xmartina <martinsprince126@gmail.com>
2024-10-20 10:17:45 +0100

    d

--- pages/staff/bursar/index.php
+++ pages/staff/bursar/index.php
@@ -31,7 +31,7 @@ include_once (__DIR__ . '/../../../partials/header.php');
                 <div class="col-md-8">
                     <div class="content">
                         <div class="text-dark p-4 rounded shadow-lg" >
-                            <div class="mb-4 font-weight-bold">
+                            <div class="mb-3" style="font-weight: bold;">
                             Sir. Peter Abuo Bisong CPA, ALGA. ASCON.
                             </div>
 
