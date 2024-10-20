b742c2543803da66bc7e7ba5b1ecdf7092f1b295
xmartina <martinsprince126@gmail.com>
2024-10-20 07:52:55 +0100

    d

---
+++ functions/staff_function.php
@@ -0,0 +1,5 @@
+<?php
+include_once (__DIR__ . '/main.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
--- pages/staff/registrar/index.php
+++ pages/staff/registrar/index.php
@@ -1,9 +1,25 @@
 <?php
-include_once (__DIR__ . '/../../../functions/department_function.php');
+$page_name = 'Registrar';
+include_once (__DIR__ . '/../../../functions/staff_function.php');
 include_once (__DIR__ . '/../../../partials/header.php');
 ?>
 <main id="main">
-<section class=" pd-section">
+    <section class="breadcumb-section relative">
+        <div class="tf-container">
+            <div class="row">
+                <div class="col-lg-12 z-index-2 relative ">
+                    <div class="title-inner center">
+                        <h1 class="title text-white mb-18"><?=$page_name?></h1>
+                        <div class="title-group text-white">
+                            <a href="<?=$siteUrl?>">Home</a>
+                            <a href="<?=$siteUrl?>pages/staff/<?=$page_name?>"><?=$page_name?></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class=" pd-section">
     <div class="tf-container">
         <div class="row quote-style">
             <div class="col-md-4">
