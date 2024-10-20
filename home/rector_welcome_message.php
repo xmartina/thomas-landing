fc974fbd3fbb976bcc5473772f7a1464c9b629d9
xmartina <martinsprince126@gmail.com>
2024-10-20 08:46:48 +0100

    d

--- home/rector_welcome_message.php
+++ home/rector_welcome_message.php
@@ -16,7 +16,8 @@
             <div class="col-12 col-sm-6 col-lg-4">
                 <div class="skill-team-image-center">
                     <img src="<?=$site_url?>assets/school_image/staff/rector.jpeg" alt="image">
-                    <div class="text-center text-dark mt-3" style="font-weight: bold; line-height: .9"><div style="font-size: 12px;">Rectors </div> Dr Francis Mowing Ganyi</div>
+                    <div class="text-center text-dark mt-3" style="font-weight: bold; line-height: .9"><div style="font-size: 12px;">Rectors
+                            <hr class="w-75"></div> Dr Francis Mowing Ganyi</div>
                 </div>
             </div>
             <div class="col-12 col-sm-6 col-lg-3">
