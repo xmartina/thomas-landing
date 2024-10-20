1bba55a4b98f53669516660bcbc4fd9e47ee3606
xmartina <martinsprince126@gmail.com>
2024-10-20 09:48:21 +0100

    d

--- pages/staff/bursar/index.php
+++ pages/staff/bursar/index.php
@@ -1,5 +1,5 @@
 <?php
-$page_name = 'Rector';
+$page_name = 'Bursar';
 include_once (__DIR__ . '/../../../functions/staff_function.php');
 include_once (__DIR__ . '/../../../partials/header.php');
 ?>
@@ -25,15 +25,32 @@ include_once (__DIR__ . '/../../../partials/header.php');
             <div class="row quote-style">
                 <div class="col-md-4">
                     <div class="image">
-                        <img src="<?= $site_url ?>assets/school_image/staff/rector.jpeg" alt="image" style="object-fit: cover;">
+                        <img src="<?= $site_url ?>assets/school_image/staff/bursar.jpeg" alt="image" style="object-fit: cover;">
                     </div>
                 </div>
                 <div class="col-md-8">
                     <div class="content">
-                        <p class="text-dark p-4 rounded shadow-lg" >" Dr. Francis Mowang Ganyi is a Nigerian by birth, a Christian of Roman Catholic extraction and a socialite with strong moral values. A retired University teacher and researcher, he is a reputable Google scholar with several publications to his credit. He has a deep interest in cultural studies with a bent for African knowledge systems and values. Married to Barr. Mrs Bridget M. Ganyi, they are blessed with children. Dr. Ganyi also displays strong interpersonal relationships and leadership drive and is a powerful public speaker. "
+                        <p class="text-dark p-4 rounded shadow-lg" >"
+                            <p class="font-weight-bold mb-2">
+                            Sir. Peter Abuo Bisong CPA, ALGA. ASCON.
+                            </p>
+                            <p class="mb-2">
+                            HOLDS A BSC FROM THE PRESTIGIOUS UNIVERSITY OF JOS NIGERIA.
+                            </p>
+                            <p class="mb-2">
+                            SERVED THE STATE AS PROCUREMENT OFFICER, FEDERAL GOVERNMENT AS KEY FIGURE IN THE FINAL ACCOUNT UNIT OF MIN. OF AVIATION AND THE LOCAL GOVERNMENT AS FINANCE ADMINISTRATOR BEFORE RETIREMENT
+                            </p>
+                            <p class="mb-2">
+                            Married with six kids
+                            </p>
+                            <p class="mb-2">
+                            Expertise
+                            Professional expertise in health, accounts.
+                            Personal development in computer, Agric, family life counseling, publishing, livelihood training and job creation. "
+                            </p>
                         </p>
                         <div class="flex-three mt-4 flex-column align-items-center name">
-                            <span style="font-size: 12px !important;text-transform: capitalize"><?=$page_name?> </span><br><span>Dr Francis Mowing Ganyi</span>
+                            <span style="font-size: 12px !important;text-transform: capitalize"><?=$page_name?> </span><br><span>Mr Peter Abuo Bisong</span>
                         </div>
 
 
