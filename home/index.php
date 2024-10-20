9e176393d774545d2c89ddfc4f82af6d2904c00d
xmartina <martinsprince126@gmail.com>
2024-10-20 08:11:36 +0100

    d

--- home/index.php
+++ home/index.php
@@ -2,5 +2,6 @@
 <?php
 include_once (__DIR__ . '/slider.php');
 include_once (__DIR__ . '/partner.php');
+include_once (__DIR__ . '/rector_welcome_message.php');
 include_once (__DIR__ . '/departments.php');
 ?>
---
+++ home/rector_welcome_message.php
@@ -0,0 +1,77 @@
+<section class="pt-130 pb-130 skill-team-member">
+    <div class="tf-container">
+        <div class="row align-center">
+            <div class=" col-md-12 col-lg-5">
+                <div class="skill-team-content">
+                    <div class="heading-title mb-40">
+                        <span class="sub-title texts-blue font-man">Best IT Solutions</span>
+                        <h2 class="title">We Prominent Truly
+                            IT Your solutions</h2>
+                        <p class="des">We denounce with righteous indignation and like men who are so
+                            beguiled and demoralized by the charms of pleasure of the moment, so blinded
+                            by desire foresee the pain</p>
+                    </div>
+                    <div class="flex progres-wrap mb-50">
+                        <div class="progress-box1 flex-three">
+                            <div class="progress-skill">
+                                <div class="couter2">
+                                    <div class="chart2" data-percent="68">
+                                        <span class="percent2">0</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="content-progress">
+                                <h5>Business <br>Strategy</h5>
+                            </div>
+                        </div>
+                        <div class="progress-box2 flex-three">
+                            <div class="progress-skill">
+                                <div class="couter2">
+                                    <div class="chart2" data-percent="93">
+                                        <span class="percent2">0</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="content-progress">
+                                <h5>Technology <br>Solutions</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="btn-wrap-about flex-three">
+                        <a href="#" class="button-src">Read More <i class="icon-angle-right"></i></a>
+                        <div class="flex-three">
+                            <div class="flex-three">
+                                <ul class="image-list flex-three">
+                                    <li>
+                                        <img src="./assets/images/avata/avt-team1.jpg" alt="Image list">
+                                    </li>
+                                    <li>
+                                        <img src="./assets/images/avata/avt-team2.jpg" alt="Image list">
+                                    </li>
+                                    <li>
+                                        <img src="./assets/images/avata/avt-team3.jpg" alt="Image list">
+                                    </li>
+                                    <li>
+                                        <img src="./assets/images/avata/avt-team4.jpg" alt="Image list">
+                                    </li>
+                                </ul>
+                            </div>
+                            <span class="font-man customer">10m+ Customers</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-4">
+                <div class="skill-team-image-center">
+                    <img src="./assets/images/team/team-skill.jpg" alt="image">
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3">
+                <div class="skill-team-image-right">
+                    <img src="./assets/images/team/team-skill1.jpg" alt="image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
