b7a5bc627255ad31cdb95caba86d600996c2ed16
xmartina <martinsprince126@gmail.com>
2024-10-22 16:22:22 +0100

    d

--- partials/breadcrumb.php
+++ partials/breadcrumb.php
@@ -1,3 +1,16 @@
+<?php
+$currentUrl = $_SERVER['REQUEST_URI'];
+
+// Check if the current request URL contains 'about'
+if (strpos($currentUrl, 'about') !== false) {
+    $parts = explode('/', trim($currentUrl, '/')); // trim removes trailing slash
+    $lastPart = end($parts);  // Get the last element of the array
+    $breadcrumb_link = $lastPart;  // Return the current URL if 'about' is found
+} elseif(strpos($currentUrl, 'staff') !== false) {
+    $breadcrumb_link = $page_name;
+}
+?>
+
 <section class="breadcumb-section relative">
     <div class="tf-container">
         <div class="row">
@@ -6,7 +19,7 @@
                     <h1 class="title text-white mb-18"><?=$page_name?></h1>
                     <div class="title-group text-white">
                         <a href="<?=$siteUrl?>">Home</a>
-                        <a href="<?=$siteUrl?>pages/staff/<?=$page_name?>"><?=$page_name?></a>
+                        <a href="<?=$siteUrl?>pages/staff/<?=$breadcrumb_link?>"><?=$page_name?></a>
                     </div>
                 </div>
             </div>
