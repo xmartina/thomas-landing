90257398cf194761b44167131f3505e4e6622c65
xmartina <martinsprince126@gmail.com>
2024-10-22 18:42:18 +0100

    d

--- pages/staff/librarian/index.php
+++ pages/staff/librarian/index.php
@@ -77,29 +77,7 @@ include_once (__DIR__ . '/../../../partials/header.php');
                         </div>
                     </article>
                 </div>
-                <div class="col-lg-4 col-12">
-                    <div class="sidebar-blog">
-                        <div class="widget mb-50">
-                            <h4 class="title-blog bb-blog mb-30">Departments</h4>
-                            <ul class="category-blog">
-                                <?php list_all_department($dep_result, $site_url) ?>
-                            </ul>
-                        </div>
-                        <div class="widget">
-                            <div class="widget-banner relative">
-                                <div class="z-index-2 relative">
-                                    <h2 class="title-banner text-white fw-300">Join the Thomas McGettrick Institute of Technology
-                                        <span class="agency fw-600">for Your Future in Technology</span>
-                                    </h2>
-                                    <p class="des text-white font-man">Take the next step in your education and explore innovative programs in IT, Engineering, and more.</p>
-                                    <div class="btn-wrap">
-                                        <a href="/admissions" class="more-link">Learn More <i class="icon-right-icon"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <?php include_once (__DIR__ . '/../../../partials/side_bar.php');  ?>
             </div>
         </div>
     </section>
