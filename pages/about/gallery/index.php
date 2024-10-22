b77659e0b6fe2cf1676e99cceb216a7ec29f9978
xmartina <martinsprince126@gmail.com>
2024-10-22 18:06:19 +0100

    d

--- pages/about/gallery/index.php
+++ pages/about/gallery/index.php
@@ -1,16 +1,18 @@
 <?php
 $page_name = "Gallery";
-include_once (__DIR__ . '/../../../functions/about_function.php');
-include_once (__DIR__ . '/../../../partials/header.php');
+include_once(__DIR__ . '/../../../functions/about_function.php');
+include_once(__DIR__ . '/../../../partials/header.php');
 ?>
-    <main id="main">
-        <?php include_once (__DIR__ . '/../../../partials/breadcrumb.php'); ?>
+<main id="main">
+    <?php include_once(__DIR__ . '/../../../partials/breadcrumb.php'); ?>
+    <section class="blog-single pd-section">
         <div class="container">
             <div class="row">
                 <?php get_gallery($gallery_result, $site_url) ?>
             </div>
         </div>
-    </main>
+    </section>
+</main>
 <?php
-include_once (__DIR__ . '/../../../partials/footer.php');
+include_once(__DIR__ . '/../../../partials/footer.php');
 ?>
