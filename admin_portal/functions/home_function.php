0fb98ad574315db19228244134acd4354a1fcba0
xmartina <martinsprince126@gmail.com>
2024-11-02 00:57:14 +0100

    d

--- admin_portal/functions/home_function.php
+++ admin_portal/functions/home_function.php
@@ -1,6 +1,6 @@
 <?php
 include_once ( __DIR__ . '/../../admin_portal/functions/main_function.php');
-session_start();
+
 $select_user_sql = "SELECT * FROM users WHERE id = '$id' "; // Adjust the table name if different
 $select_user_result = $conn->query($select_user_sql);
 $row = $select_user_result->fetch_assoc();
--- admin_portal/index.php
+++ admin_portal/index.php
@@ -1,4 +1,6 @@
-<?php include __DIR__ . '/../admin_portal/partials/session.php'; ?>
+<?php include __DIR__ . '/../admin_portal/partials/session.php';
+echo $id;
+?>
 <?php include_once (__DIR__ . '/../admin_portal/functions/home_function.php'); ?>
 <?php include __DIR__ . '/../admin_portal/partials/main.php'; ?>
 <head>
