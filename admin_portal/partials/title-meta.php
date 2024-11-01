3c14c31403e530557b0dea0ec72605417c62b040
xmartina <martinsprince126@gmail.com>
2024-11-02 00:30:47 +0100

    d

--- admin_portal/partials/title-meta.php
+++ admin_portal/partials/title-meta.php
@@ -1,3 +1,4 @@
+<?php include_once(__DIR__.'/../functions/main_function.php'); ?>
 
 <meta charset="utf-8" />
 <title><?= ($title) ? $title : '' ?> | Morvin - Admin & Dashboard Template</title>
@@ -5,4 +6,4 @@
 <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
 <meta content="Themesdesign" name="author" />
 <!-- App favicon -->
-<link rel="shortcut icon" href="<?= $site_url  ?>assets/images/favicon.png">
+<link rel="shortcut icon" href="<?= $site_url ?>assets/images/favicon.png">
