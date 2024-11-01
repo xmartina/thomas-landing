401c34525b071ff949df72c25ec3e465d29a74c5
xmartina <martinsprince126@gmail.com>
2024-11-02 00:07:10 +0100

    d

--- admin_portal/partials/head-css.php
+++ admin_portal/partials/head-css.php
@@ -4,3 +4,8 @@
 <link href="<?= $admin_url  ?>assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 <!-- App Css-->
 <link href="<?= $admin_url  ?>assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+<style>
+    .bg-primary{
+        background-color: #057a0f !important;
+    }
+</style>
