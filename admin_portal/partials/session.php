459abe776ffbf2f4d0fef32e6fae6bb5e17ae814
xmartina <martinsprince126@gmail.com>
2024-11-01 22:32:28 +0100

    d

--- admin_portal/partials/session.php
+++ admin_portal/partials/session.php
@@ -3,8 +3,8 @@
 session_start();
 
 // Check if the user is logged in, if not then redirect him to login page
-// if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-//      header("location: auth-login.php");
-//     exit;
-//  }
+ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+      header("location: auth-login.php");
+     exit;
+  }
 ?>
