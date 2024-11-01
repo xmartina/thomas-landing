41447db646f6d904ab13545676f53eb401621bae
xmartina <martinsprince126@gmail.com>
2024-11-01 23:58:53 +0100

    d

--- admin_portal/functions/main_function.php
+++ admin_portal/functions/main_function.php
@@ -18,4 +18,4 @@ $favicon = $row["favicon"];
 $footer_dec = $row["footer_dec"];
 $footer_copyright = $row["footer_copyright"];
 $site_url = $siteUrl;
-$admin_url = $site_url . 'admin_portal';
+$admin_url = $site_url . 'admin_portal/';
