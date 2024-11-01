cdc8df5b9d305f5a453c6028eea6f86d78a5ec34
xmartina <martinsprince126@gmail.com>
2024-11-02 00:39:09 +0100

    d

--- admin_portal/partials/title-meta.php
+++ admin_portal/partials/title-meta.php
@@ -1,11 +1,13 @@
 
 <?php
-ob_start();
-include(__DIR__.'/../../admin_portal/functions/main_function.php');
+$site_name = 'Thomas McGettrick Institute of Technology';
+$site_dec = 'Excellence & Technology';
+$site_url = 'https://tmit.com.ng/';
+
 ?>
 
 <meta charset="utf-8" />
-<title><?= ($title) ? $title : '' ?> | <?= $site_title ?></title>
+<title><?= ($title) ? $title : '' ?> | <?= $site_dec ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta content="<?= $site_dec ?>" name="description" />
 <meta content="<?= $site_name ?>" name="author" />
