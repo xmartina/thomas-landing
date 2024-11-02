9d4ce9af03d0292a3b9397bb40db9505b6c6e66d
xmartina <martinsprince126@gmail.com>
2024-11-02 01:29:36 +0100

    d

--- admin_portal/functions/home_function.php
+++ admin_portal/functions/home_function.php
@@ -1,7 +1,23 @@
 <?php
 include_once ( __DIR__ . '/../../admin_portal/functions/main_function.php');
 
-$select_user_sql = "SELECT * FROM users WHERE id = '$id' "; // Adjust the table name if different
+$select_user_sql = "SELECT * FROM users WHERE id = '$id'";
 $select_user_result = $conn->query($select_user_sql);
-$row = $select_user_result->fetch_assoc();
-$f_name = $row['fname'];
+
+// Check if the query executed successfully and returned a result
+if ($select_user_result && $select_user_result->num_rows > 0) {
+    // Fetch the associative array of the user data
+    $row = $select_user_result->fetch_assoc();
+
+    // Assign user data to variables
+    $f_name = $row['fname'];
+    $l_name = $row['lname'];
+    $useremail = $row['useremail'];
+    $username = $row['username'];
+    $phone_number = $row['phone_number'];
+    $profile_img = $row['profile_img'];
+    $token = $row['token'];
+    $role = $row['role'];
+} else {
+    echo "No user found with the specified ID.";
+}
