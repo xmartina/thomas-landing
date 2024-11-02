90bbbfecfe87a2cc8c8e1f6ad239535401d3fb03
xmartina <martinsprince126@gmail.com>
2024-11-02 13:46:15 +0100

    d

--- admin_portal/partials/topbar.php
+++ admin_portal/partials/topbar.php
@@ -4,19 +4,21 @@
 
                <!-- LOGO -->
          <div class="navbar-brand-box">
-            <a href="index.php" class="logo logo-dark">
+            <a href="<?= $admin_url ?>" class="logo logo-dark">
                 <span class="logo-sm">
                     <img src="assets/images/logo-sm.png" alt="" height="22">
+
                 </span>
                 <span class="logo-lg">
-                    <img src="assets/images/logo-dark.png" alt="" height="20">
+<!--                    <img src="assets/images/logo-dark.png" alt="" height="20">-->
+                    <span class="text-light">TMIT</span>
                 </span>
             </a>
 
-            <a href="index.php" class="logo logo-light">
+            <a href="<?= $admin_url ?>" class="logo logo-light">
                 <span class="logo-sm">
-<!--                    <img src="assets/images/logo-sm.png" alt="" height="22">-->
-                    <span class="text-white">TMIT</span>
+                    <img src="assets/images/logo-sm.png" alt="" height="22">
+<!--                    <span class="text-white">TMIT</span>-->
                 </span>
                 <span class="logo-lg">
 <!--                    <img src="assets/images/logo-light.png" alt="" height="20">-->
