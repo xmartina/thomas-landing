2c70bff4fc1399179ad551341df861d366807f2b
xmartina <martinsprince126@gmail.com>
2024-11-02 01:19:32 +0100

    d

--- admin_portal/index.php
+++ admin_portal/index.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/../admin_portal/partials/session.php';?>
 <?php include __DIR__ . '/../admin_portal/partials/main.php';
-echo $_SESSION["id"];
+$_SESSION["id"] = $id;
 ?>
 <?php include_once (__DIR__ . '/../admin_portal/functions/home_function.php'); ?>
 <head>
