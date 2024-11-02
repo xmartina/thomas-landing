b142ed31af399485d909838732a89f56b0bc998f
xmartina <martinsprince126@gmail.com>
2024-11-02 14:28:29 +0100

    d

--- admin_portal/partials/footer.php
+++ admin_portal/partials/footer.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Morvin.
+                <script>document.write(new Date().getFullYear())</script> © All Rights Reserved
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                    <?=$site_name?>
                 </div>
             </div>
         </div>
