9358dc3de1b64666499a1126d71fafc879dfae36
xmartina <martinsprince126@gmail.com>
2024-11-03 12:55:28 +0100

    d

---
+++ admin_portal/partials/footer.php
@@ -0,0 +1,68 @@
+<?php include_once __DIR__.'/../partials/headersearch_modal.php';?>
+<!-- Footer Start -->
+<footer class="footer mt-auto py-3 bg-white text-center">
+    <div class="container">
+        <span class="text-muted"> Copyright © <span id="year"></span> <a
+                href="javascript:void(0);" class="text-dark fw-semibold">Ynex</a>.
+            Designed with <span class="bi bi-heart-fill text-danger"></span> by <a href="javascript:void(0);">
+                <span class="fw-semibold text-primary text-decoration-underline">Spruko</span>
+            </a> All
+            rights
+            reserved
+        </span>
+    </div>
+</footer>
+<!-- Footer End -->
+</div>
+
+<div class="scrollToTop">
+    <span class="arrow"><i class="ri-arrow-up-s-fill fs-20"></i></span>
+</div>
+<div id="responsive-overlay"></div>
+
+<!-- Popper JS -->
+<script src="../assets/libs/@popperjs/core/umd/popper.min.js"></script>
+
+<!-- Bootstrap JS -->
+<script src="../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Defaultmenu JS -->
+<script src="../assets/js/defaultmenu.min.js"></script>
+
+<!-- Node Waves JS-->
+<script src="../assets/libs/node-waves/waves.min.js"></script>
+
+<!-- Sticky JS -->
+<script src="../assets/js/sticky.js"></script>
+
+<!-- Simplebar JS -->
+<script src="../assets/libs/simplebar/simplebar.min.js"></script>
+<script src="../assets/js/simplebar.js"></script>
+
+<!-- Color Picker JS -->
+<script src="../assets/libs/@simonwep/pickr/pickr.es5.min.js"></script>
+
+<!-- JSVector Maps JS -->
+<script src="../assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
+
+<!-- JSVector Maps MapsJS -->
+<script src="../assets/libs/jsvectormap/maps/world-merc.js"></script>
+
+<!-- Apex Charts JS -->
+<script src="../assets/libs/apexcharts/apexcharts.min.js"></script>
+
+<!-- Chartjs Chart JS -->
+<script src="../assets/libs/chart.js/chart.min.js"></script>
+
+<!-- CRM-Dashboard -->
+<script src="../assets/js/crm-dashboard.js"></script>
+
+<!-- Custom-Switcher JS -->
+<script src="../assets/js/custom-switcher.min.js"></script>
+
+<!-- Custom JS -->
+<script src="../assets/js/custom.js"></script>
+
+</body>
+
+</html>
