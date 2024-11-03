84ffb431d5e4314bd3c5f893c9319c03c23aa615
xmartina <martinsprince126@gmail.com>
2024-11-03 22:50:15 +0100

    d

--- admin_portal/index.php
+++ admin_portal/index.php
@@ -66,64 +66,68 @@ include_once __DIR__.'/../admin_portal/partials/header.php';
                                         </tr>
                                         </thead>
                                         <tbody>
-<?php
-while ($row = $get_new_users_result->fetch_assoc())
-{
-    $first_name = $row['firstname'];
-    $last_name = $row['lastname'];
-    $gender = $row['gender'];
-    $state = $row['state'];
-    $last_class_passed = $row['last_class_passed'];
-    $passport = $row['passport'];
-    $reg_number = $row['reg_number'];
-    $date_of_reg  = $row['date_of_reg'];
+                                        <?php
+                                        while ($row = $get_new_users_result->fetch_assoc()) {
+                                            $first_name = $row['firstname'];
+                                            $last_name = $row['lastname'];
+                                            $gender = $row['gender'];
+                                            $state = $row['state'];
+                                            $last_class_passed = $row['last_class_passed'];
+                                            $passport = $row['passport'];
+                                            $reg_number = $row['reg_number'];
+                                            $date_of_reg = $row['date_of_reg'];
 
-    $full_name = $first_name.' '.$last_name;
+                                            $full_name = $first_name . ' ' . $last_name;
 
-    ?>
-                                        <tr>
-                                            <th scope="row" class="ps-4"><input class="form-check-input" type="checkbox" id="checkboxNoLabel2" value="" aria-label="..."></th>
-                                            <td>
-                                                <div class="d-flex align-items-center fw-semibold">
-                                                                <span class="avatar avatar-sm me-2 avatar-rounded">
-                                                                    <img src="https://portal.tmit.com.ng/<?=$passport?>" alt="img">
-                                                                </span><?=$full_name?>
-                                                </div>
-                                            </td>
-                                            <td><?=$reg_number?></td>
-                                            <td><?=$last_class_passed?></td>
-                                            <td>
-                                                <span id="badge-state" class="badge bg-info-transparent"><?= $state ?></span>
-                                            </td>
+                                            // Generate a unique ID for the badge using the reg_number or another unique field
+                                            $badgeId = 'badge-state-' . $reg_number;
+                                            ?>
+                                            <tr>
+                                                <th scope="row" class="ps-4"><input class="form-check-input" type="checkbox" id="checkboxNoLabel2" value="" aria-label="..."></th>
+                                                <td>
+                                                    <div class="d-flex align-items-center fw-semibold">
+                <span class="avatar avatar-sm me-2 avatar-rounded">
+                    <img src="https://portal.tmit.com.ng/<?= $passport ?>" alt="img">
+                </span><?= $full_name ?>
+                                                    </div>
+                                                </td>
+                                                <td><?= $reg_number ?></td>
+                                                <td><?= $last_class_passed ?></td>
+                                                <td>
+                                                    <span id="<?= $badgeId ?>" class="badge bg-info-transparent"><?= $state ?></span>
+                                                </td>
 
-                                            <script>
-                                                // JavaScript array of possible badge classes
-                                                const badgeClasses = [
-                                                    'badge bg-info-transparent',
-                                                    'badge bg-success-transparent',
-                                                    'badge bg-danger-transparent',
-                                                    'badge bg-primary-transparent'
-                                                ];
+                                                <script>
+                                                    // JavaScript array of possible badge classes
+                                                    const badgeClasses = [
+                                                        'badge bg-info-transparent',
+                                                        'badge bg-success-transparent',
+                                                        'badge bg-danger-transparent',
+                                                        'badge bg-primary-transparent'
+                                                    ];
 
-                                                // Get the badge element
-                                                const badgeElement = document.getElementById('badge-state');
+                                                    // Get the badge element by its unique ID
+                                                    const badgeElement = document.getElementById("<?= $badgeId ?>");
 
-                                                // Randomly select a class from the badgeClasses array
-                                                const randomBadgeClass = badgeClasses[Math.floor(Math.random() * badgeClasses.length)];
+                                                    // Randomly select a class from the badgeClasses array
+                                                    const randomBadgeClass = badgeClasses[Math.floor(Math.random() * badgeClasses.length)];
 
-                                                // Apply the selected class to the badge element
-                                                badgeElement.className = randomBadgeClass;
-                                            </script>
+                                                    // Apply the selected class to the badge element
+                                                    badgeElement.className = randomBadgeClass;
+                                                </script>
 
-                                            <td><?=$date_of_reg ?></td>
-                                            <td>
-                                                <div class="hstack gap-2 fs-15">
-                                                    <a aria-label="anchor" href="javascript:void(0);" class="btn btn-icon btn-wave waves-effect waves-light btn-sm btn-success-light"><i class="ri-download-2-line"></i></a>
-                                                    <a onclick="location.href='https://portal.tmit.com.ng/resources/view/modules.php?view_students'" aria-label="anchor" href="javascript:void(0);" class="btn btn-icon btn-wave waves-effect waves-light btn-sm btn-primary-light"><i class="ri-edit-line"></i></a>
-                                                </div>
-                                            </td>
-                                        </tr>
-    <?php } ?>
+                                                <td><?= $date_of_reg ?></td>
+                                                <td>
+                                                    <div class="hstack gap-2 fs-15">
+                                                        <a aria-label="anchor" href="javascript:void(0);" class="btn btn-icon btn-wave waves-effect waves-light btn-sm btn-success-light"><i class="ri-download-2-line"></i></a>
+                                                        <a onclick="location.href='https://portal.tmit.com.ng/resources/view/modules.php?view_students'" aria-label="anchor" href="javascript:void(0);" class="btn btn-icon btn-wave waves-effect waves-light btn-sm btn-primary-light"><i class="ri-edit-line"></i></a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <?php
+                                        }
+                                        ?>
+
                                         </tbody>
                                     </table>
                                 </div>
