888cbd5269e08c104b4f2cefa55be9effbc1b635
xmartina <martinsprince126@gmail.com>
2024-11-03 18:11:01 +0100

    d

--- admin_portal/functions/auth/session.php
+++ admin_portal/functions/auth/session.php
@@ -1,23 +1,23 @@
 <?php
-//// Initialize the session
-//$url = $_SERVER['REQUEST_URI'];
-//
-//// Check if the current URL does not contain 'login'
-//if (strpos($url, 'login') === false) {
-//    // If the user is not logged in, redirect to the login page
-//    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-//        header("Location:".$admin_url."auth/login");
-//        exit;
-//    }
-//}
-//// Check if the URL does contain 'login'
-//elseif (strpos($url, 'login') !== false) {
-//    // If the user is already logged in, redirect to the admin portal
-//    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-//        header("Location:".$admin_url);
-//        exit;
-//    }
-//}
-//
-//
-//?>
+// Initialize the session
+$url = $_SERVER['REQUEST_URI'];
+
+// Check if the current URL does not contain 'login'
+if (strpos($url, 'login') === false) {
+    // If the user is not logged in, redirect to the login page
+    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+        header("Location:".$admin_url."auth/login");
+        exit;
+    }
+}
+// Check if the URL does contain 'login'
+elseif (strpos($url, 'login') !== false) {
+    // If the user is already logged in, redirect to the admin portal
+    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+        header("Location:".$admin_url);
+        exit;
+    }
+}
+
+
+?>
