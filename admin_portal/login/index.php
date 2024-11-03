74667d7a175271550209ba336fa099ebf999260b
xmartina <martinsprince126@gmail.com>
2024-11-03 18:07:35 +0100

    d

--- admin_portal/login/index.php
+++ admin_portal/login/index.php
@@ -1,4 +1,6 @@
-<?php include_once __DIR__ .'/../../admin_portal/functions/auth/login_function.php';
+<?php
+session_start();
+include_once __DIR__ .'/../../admin_portal/functions/auth/login_function.php';
 include_once __DIR__.'/../../admin_portal/partials/header.php';
 ?>
 
