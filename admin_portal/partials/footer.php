6072c5e44a52ff4d9a198b5a47d2690622905775
xmartina <martinsprince126@gmail.com>
2024-11-03 13:23:38 +0100

    d

--- admin_portal/partials/footer.php
+++ admin_portal/partials/footer.php
@@ -21,47 +21,47 @@
 <div id="responsive-overlay"></div>
 
 <!-- Popper JS -->
-<script src="../assets/libs/@popperjs/core/umd/popper.min.js"></script>
+<script src="<?= $site_url ?>assets/libs/@popperjs/core/umd/popper.min.js"></script>
 
 <!-- Bootstrap JS -->
-<script src="../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= $site_url ?>assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Defaultmenu JS -->
-<script src="../assets/js/defaultmenu.min.js"></script>
+<script src="<?= $site_url ?>assets/js/defaultmenu.min.js"></script>
 
 <!-- Node Waves JS-->
-<script src="../assets/libs/node-waves/waves.min.js"></script>
+<script src="<?= $site_url ?>assets/libs/node-waves/waves.min.js"></script>
 
 <!-- Sticky JS -->
-<script src="../assets/js/sticky.js"></script>
+<script src="<?= $site_url ?>assets/js/sticky.js"></script>
 
 <!-- Simplebar JS -->
-<script src="../assets/libs/simplebar/simplebar.min.js"></script>
-<script src="../assets/js/simplebar.js"></script>
+<script src="<?= $site_url ?>assets/libs/simplebar/simplebar.min.js"></script>
+<script src="<?= $site_url ?>assets/js/simplebar.js"></script>
 
 <!-- Color Picker JS -->
-<script src="../assets/libs/@simonwep/pickr/pickr.es5.min.js"></script>
+<script src="<?= $site_url ?>assets/libs/@simonwep/pickr/pickr.es5.min.js"></script>
 
 <!-- JSVector Maps JS -->
-<script src="../assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
+<script src="<?= $site_url ?>assets/libs/jsvectormap/js/jsvectormap.min.js"></script>
 
 <!-- JSVector Maps MapsJS -->
-<script src="../assets/libs/jsvectormap/maps/world-merc.js"></script>
+<script src="<?= $site_url ?>assets/libs/jsvectormap/maps/world-merc.js"></script>
 
 <!-- Apex Charts JS -->
-<script src="../assets/libs/apexcharts/apexcharts.min.js"></script>
+<script src="<?= $site_url ?>assets/libs/apexcharts/apexcharts.min.js"></script>
 
 <!-- Chartjs Chart JS -->
-<script src="../assets/libs/chart.js/chart.min.js"></script>
+<script src="<?= $site_url ?>assets/libs/chart.js/chart.min.js"></script>
 
 <!-- CRM-Dashboard -->
-<script src="../assets/js/crm-dashboard.js"></script>
+<script src="<?= $site_url ?>assets/js/crm-dashboard.js"></script>
 
 <!-- Custom-Switcher JS -->
-<script src="../assets/js/custom-switcher.min.js"></script>
+<script src="<?= $site_url ?>assets/js/custom-switcher.min.js"></script>
 
 <!-- Custom JS -->
-<script src="../assets/js/custom.js"></script>
+<script src="<?= $site_url ?>assets/js/custom.js"></script>
 
 </body>
 
