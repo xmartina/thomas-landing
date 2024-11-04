fe24240011eeb037b2cc4f99026d76be0a783090
xmartina <martinsprince126@gmail.com>
2024-11-04 12:41:02 +0100

    d

--- home/rector_welcome_message.php
+++ home/rector_welcome_message.php
@@ -1,36 +1,55 @@
 <section class="pt-130 skill-team-member" style="margin-top: 140px;">
     <div class="container">
+        <?php
+        $sql = "SELECT * FROM rector_welcome_note WHERE id = 1";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $sub_title = $row['sub_title'];
+                $title = $row['title'];
+                $content = $row['content'];
+                $button_text = $row['button_text'];
+                $button_link = $row['button_link'];
+                $data_wow_delay = $row['data-wow-delay'];
+                $rec_img = $row['rec_img'];
+                $rec_position = $row['rec_position'];
+                $rec_name = $row['rec_name'];
+                $extra_img = $row['extra_img'];
+
+        ?>
         <div class="row align-center">
             <div class=" col-md-12 col-lg-5">
                 <div class="skill-team-content">
                     <div class="heading-title mb-40">
                         <span class="sub-title texts-blue font-man"><?= $site_name ?></span>
-                        <h2 class="title">Rector's Welcome Note</h2>
-                        <p class="des">As Rector of Thomas McCGetrick lnstitute of Technology Ogoja, l welcome all those who visit this website for information and other purposes concerning the lnstitute which prides by the mantra "Outstandingly Different." You are sure to find the difference in Thomas McCGetrick lnstitute of Technology. Feel at home with us.</p>
+                        <h2 class="title"><?= $title ?></h2>
+                        <p class="des"><?= $content ?></p>
                     </div>
                     <div class="btn-wrap-about flex-three">
-                        <a href="<?=$site_url?>pages/about/rector_welcome_message" class="button-src">Read More <i class="icon-angle-right"></i></a>
+                        <a href="<?=$site_url?>pages/about/<?= $button_link ?>" class="button-src"><?= $button_text ?> <i class="icon-angle-right"></i></a>
                     </div>
                 </div>
             </div>
             <div class="col-12 col-sm-6 col-lg-4">
                 <div class="skill-team-image-center">
-                    <img src="<?=$site_url?>assets/school_image/staff/rector.jpeg" alt="image">
+                    <img src="<?=$site_url?>assets/school_image/staff/<?= $rec_img ?>" alt="image">
                     <div class="text-center d-flex justify-content-center align-items-center flex-column text-dark mt-3" style="font-weight: bold; line-height: .8">
                         <div style="font-size: 12px;">
-                            Rector
+                            <?= $rec_position ?>
                         </div>
                         <hr class="py-2 w-75">
-                        Dr Francis Mowing Ganyi
+                        <?= $rec_name ?>
                     </div>
                 </div>
             </div>
             <div class="col-12 col-sm-6 col-lg-3">
                 <div class="skill-team-image-right">
-                    <img src="<?=$siteUrl?>assets/images/logo.png" alt="image">
+                    <img src="<?=$siteUrl?>assets/images/<?= $extra_img ?>" alt="image">
                 </div>
             </div>
         </div>
+        <?php } }?>
     </div>
 
 </section>
