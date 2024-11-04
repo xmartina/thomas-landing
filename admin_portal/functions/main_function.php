5a428f5c7c7c22b15d06e7ab80b03b264088952b
xmartina <martinsprince126@gmail.com>
2024-11-04 01:02:18 +0100

    d

--- admin_portal/functions/main_function.php
+++ admin_portal/functions/main_function.php
@@ -29,31 +29,30 @@ $sitelogo = $site_logo;
 include_once __DIR__.'/../../admin_portal/functions/auth/session.php';
 if (isset($_SESSION['user_id'])) {
     $id = $_SESSION['user_id'];
+
+    $select_user_sql = "SELECT * FROM users WHERE id = '$id'";
+    $select_user_result = $conn->query($select_user_sql);
+
+    if ($select_user_result && $select_user_result->num_rows > 0) {
+        $row = $select_user_result->fetch_assoc();
+
+        $f_name = $row['fname'];
+        $l_name = $row['lname'];
+        $useremail = $row['useremail'];
+        $username = $row['username'];
+        $phone_number = $row['phone_number'];
+        $profile_img = $row['profile_img'];
+        $token = $row['token'];
+        $role = $row['role'];
+    } else {
+        echo "No user found with the specified ID.";
+    }
+    $full_name = $f_name . ' ' . $l_name;
+    if ($role = 1) {
+        $user_role = 'Super Administrator';
+    } elseif ($role = 2) {
+        $user_role = 'Administrator';
+    } elseif ($role = 3) {
+        $user_role = 'Moderator';
+    }
 }
-$select_user_sql = "SELECT * FROM users WHERE id = '$id'";
-$select_user_result = $conn->query($select_user_sql);
-
-if ($select_user_result && $select_user_result->num_rows > 0) {
-    $row = $select_user_result->fetch_assoc();
-
-    $f_name = $row['fname'];
-    $l_name = $row['lname'];
-    $useremail = $row['useremail'];
-    $username = $row['username'];
-    $phone_number = $row['phone_number'];
-    $profile_img = $row['profile_img'];
-    $token = $row['token'];
-    $role = $row['role'];
-} else {
-    echo "No user found with the specified ID.";
-}
-$full_name = $f_name . ' ' . $l_name;
-if ($role = 1) {
-    $user_role = 'Super Administrator';
-} elseif ($role = 2) {
-    $user_role = 'Administrator';
-} elseif ($role = 3) {
-    $user_role = 'Moderator';
-}
-
-
